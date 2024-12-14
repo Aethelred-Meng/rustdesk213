c90b9f7e8e467826ea592609a539d14ad126f184
Aethelred-Meng <70083307+Aethelred-Meng@users.noreply.github.com>
2024-12-14 10:17:08 +0800

    connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -78,9 +78,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
 
     setupServerWidget() => Flexible(
           child: Offstage(
-            offstage: !(!_svcStopped.value &&
-                stateGlobal.svcStatus.value == SvcStatus.ready &&
-                _svcIsUsingPublicServer.value),
+            offstage: true, // 修改这里，始终隐藏提示
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
